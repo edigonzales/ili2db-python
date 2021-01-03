584ecd56eb5455e0e5ea876d88cd87d8ada2ff1a
Stefan Ziegler <edi.gonzales@gmail.com>
2021-01-03 20:17:41 +0100

    clean code / add graalvm example

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ Classpath separator:
 - Windows `;`
 
 ## GraalVM
+1. Install GraalVM, e.g. with https://sdkman.io/
+2. Install Python `gu install Python`
 
 ## jython-simple
 ```
@@ -32,4 +34,17 @@ Runs successully the first time and the gpkg file is created.
 Throws an exception since it tries to insert the settings a second time:
 ```
 ./ili2db.py
+```
+
+## graalvm-simple
+```
+cd graalvm-simple
+```
+
+```
+export CLASSPATH=~/apps/ili2gpkg-4.4.5/ili2gpkg-4.4.5.jar:~/apps/ili2gpkg-4.4.5/libs/antlr-2.7.7.jar:~/apps/ili2gpkg-4.4.5/libs/base64-2.3.9.jar:~/apps/ili2gpkg-4.4.5/libs/ehibasics-1.4.0.jar:~/apps/ili2gpkg-4.4.5/libs/ehisqlgen-1.13.8.jar:~/apps/ili2gpkg-4.4.5/libs/ili2c-core-5.1.5.jar:~/apps/ili2gpkg-4.4.5/libs/ili2c-tool-5.1.5.jar:~/apps/ili2gpkg-4.4.5/libs/ili2db-4.4.5.jar:~/apps/ili2gpkg-4.4.5/libs/iox-api-1.0.3.jar:~/apps/ili2gpkg-4.4.5/libs/iox-ili-1.21.4.jar:~/apps/ili2gpkg-4.4.5/libs/jackson-core-2.9.7.jar:~/apps/ili2gpkg-4.4.5/libs/jts-core-1.14.0.jar:~/apps/ili2gpkg-4.4.5/libs/sqlite-jdbc-3.8.11.2.jar  
+```
+
+```
+graalpython --jvm --vm.cp=$CLASSPATH ili2db.py
 ```
--- graalvm-simple/ili2db.py
+++ graalvm-simple/ili2db.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env jython
-import sys
+import java
 
-from ch.ehi.ili2db.base import Ili2db
-from ch.ehi.ili2db.base import Ili2dbException
-from ch.ehi.ili2db.gui import Config
-from ch.ehi.ili2gpkg import GpkgMain
-
-print sys.path
+Config = java.type('ch.ehi.ili2db.gui.Config')
+Ili2db = java.type('ch.ehi.ili2db.base.Ili2db')
+Ili2dbException = java.type('ch.ehi.ili2db.base.Ili2dbException')
+GpkgMain = java.type('ch.ehi.ili2gpkg.GpkgMain')
 
 settings = Config()
 GpkgMain().initConfig(settings)
@@ -17,6 +14,10 @@ settings.setDefaultSrsCode("2056")
 settings.setNameOptimization(Config.NAME_OPTIMIZATION_TOPIC)
 settings.setCreateEnumDefs(Config.CREATE_ENUM_DEFS_MULTI)
 settings.setDbfile("254900.gpkg")
+# https://github.com/claeis/ili2db/blob/11e48f5493ebaef6bd7613522ddf1725cf7f244e/src/ch/ehi/ili2db/gui/Config.java#L493
+# Es gibt zwei Methoden mit dem gleichen Namen. Eine davon ist statisch.
+# settings.setStrokeArcs(Config.STROKE_ARCS_ENABLE)
+# oder
 Config.setStrokeArcs(settings, Config.STROKE_ARCS_ENABLE)
 settings.setValidation(False)
 settings.setItfTransferfile(True)
@@ -24,6 +25,9 @@ settings.setDburl("jdbc:sqlite:" + settings.getDbfile())
 settings.setXtffile("254900.itf")
 try:
     Ili2db.run(settings, None)
-except Ili2dbException, value: 
-    print "fubar" 
-    print value
+except Ili2dbException as value: 
+    print(fubar)
+    print(value)
+
+
+
